db2c0af1bb87a23e243e637f4e21f1c62a3bfbbc
Ingrith Reis <ingrith.reis@hotmail.com>
2021-11-18 17:58:55 -0300

    Primeiro ModelView Controller Departament

---
+++ SalesWebMVC/Controllers/DepartamentsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Controllers
+{
+    public class DepartamentsController : Controller
+    {
+        
+        
+        public IActionResult Index()
+        {
+            List<Departament> departaments = new List<Departament>();
+            departaments.Add(new Departament
+            {
+                Id = 1,
+                Name = "Eletronics"
+            });
+
+            departaments.Add(new Departament
+            {
+                Id = 2,
+                Name = "Fashion"
+            });
+
+
+            return View(departaments);
+        }
+    }
+}
