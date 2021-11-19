d6780e74e20ad8e6f716c3ebd59053a83847c3dc
Ingrith Reis <ingrith.reis@hotmail.com>
2021-11-19 18:17:47 -0300

    Seeding Service

--- SalesWebMVC/Data/SeedingService.cs
+++ SalesWebMVC/Data/SeedingService.cs
@@ -28,18 +28,18 @@ namespace SalesWebMVC.Data
             Departament d5= new Departament("Fashion");
             Departament d4 = new Departament( "Books");
 
-            Seller s1 = new Seller("Vanessa", "vanessa@teste",new DateTime(13,10,1980), 1000.00, d1);
-            Seller s2 = new Seller( "Rodrigo Campos", "Roro@teste", new DateTime(11,05, 1990), 1100.00, d2);
-            Seller s3 = new Seller("Marieta", "Marimar@teste", new DateTime(13, 10, 1980), 1000.00, d1);
-            Seller s4 = new Seller("Arnaldo Antunes", "Arnaldo@teste", new DateTime(03, 05, 1979), 2000.00, d4);
-            Seller s5 = new Seller("Pablo", "Vitar@teste", new DateTime(15, 04, 1995), 1000.00, d5);
-            Seller s6 = new Seller("Joana", "Jojo@teste", new DateTime(25, 03, 1970), 1350.00, d2);
-            Seller s7 = new Seller( "Flavia", "Flafla@teste", new DateTime(12, 02, 1991), 1450.00, d4);
-            Seller s8 = new Seller("Almir", "Almir@teste", new DateTime(28, 10, 1985), 1654.00, d1);
+            Seller s1 = new Seller("Vanessa", "vanessa@teste",new DateTime(1980,10,1), 1000.00, d1);
+            Seller s2 = new Seller( "Rodrigo Campos", "Roro@teste", new DateTime(1980, 10, 1), 1100.00, d2);
+            Seller s3 = new Seller("Marieta", "Marimar@teste", new DateTime(1980, 10, 1), 1000.00, d1);
+            Seller s4 = new Seller("Arnaldo Antunes", "Arnaldo@teste", new DateTime(1980, 10, 1), 2000.00, d4);
+            Seller s5 = new Seller("Pablo", "Vitar@teste", new DateTime(1980, 10, 1), 1000.00, d5);
+            Seller s6 = new Seller("Joana", "Jojo@teste", new DateTime(1980, 10, 1), 1350.00, d2);
+            Seller s7 = new Seller( "Flavia", "Flafla@teste", new DateTime(1980, 10, 1), 1450.00, d4);
+            Seller s8 = new Seller("Almir", "Almir@teste", new DateTime(1980, 10, 1), 1654.00, d1);
 
-            SalesRecord r1 = new SalesRecord(new DateTime(25, 10, 2021), 400.00, Models.Enuns.SaleStatus.Billed, s3);
-            SalesRecord r2 = new SalesRecord(new DateTime(30, 10, 2021), 11000.00, Models.Enuns.SaleStatus.Billed, s1);
-            SalesRecord r3 = new SalesRecord(new DateTime(14,10,  2021), 25000.00, Models.Enuns.SaleStatus.Billed, s4);
+            SalesRecord r1 = new SalesRecord(new DateTime(2021,10,05), 400.00, Models.Enuns.SaleStatus.Billed, s3);
+            SalesRecord r2 = new SalesRecord(new DateTime(2021, 10,03), 11000.00, Models.Enuns.SaleStatus.Billed, s1);
+            SalesRecord r3 = new SalesRecord(new DateTime(2021,05,21), 25000.00, Models.Enuns.SaleStatus.Billed, s4);
 
             _context.Departament.AddRange(d1, d2, d5, d4);
 
