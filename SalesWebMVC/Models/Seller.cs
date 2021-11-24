f10ff32c48be9a8b644669ed12c962639a464faf
Ingrith Reis <ingrith.reis@hotmail.com>
2021-11-24 17:21:06 -0300

    Validação

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -9,21 +9,29 @@ namespace SalesWebMVC.Models
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "{0} Campo obrigatório!")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage ="O {0} deve conter entre {2} a {1} caracteres")]
         [Display(Name = "Nome")]
         public string Name{ get; set; }
 
+        [Required(ErrorMessage = "{0} Campo obrigatório!")]
+        [EmailAddress(ErrorMessage ="Endereço de email inválido!")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} Campo obrigatório!")]
         [Display(Name ="Data de nascimento")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "{0} Campo obrigatório!")]
+        [Range(1000.0, 50000.0, ErrorMessage = "{0} deve ter valor máximo de {2} e mínimo de {1} Reais")]
         [Display(Name = "Base Salarial")]
         [DisplayFormat(DataFormatString ="{0:F2}")] // duas casas decimais 
         public double BaseSalary{ get; set; }
 
+        
         [Display(Name = "Departamento")]
         public Departament Departament { get; set; }
         public int DepartamentId { get; set; }
