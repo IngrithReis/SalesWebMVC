a7fc93fff431cc7a2f58a8eb3f2f74f1a12df09f
Ingrith Reis <ingrith.reis@hotmail.com>
2021-11-25 16:05:04 -0300

    Preparando a navegação de telas de pesquisa de vendas

---
+++ SalesWebMVC/Controllers/SalesRecordsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        private readonly SellerService _sellerService;
+        private readonly DepartamentService _departamentService;
+
+        public SalesRecordsController(SellerService sellerService, DepartamentService departamentService)
+        {
+            _sellerService = sellerService;
+            _departamentService = departamentService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+
+
+    }
+}
